1482e230599a24d14ec6900bc1fddfd3512924a0
bcabanes <contact@benjamincabanes.com>
2017-06-16 17:52:03 -0400

    feat(app): Updates routes for lazy loading & imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: 'home', component: HomeComponent },
+  { path: 'blog', loadChildren: 'app/blog/blog.module#BlogModule' },
+  { path: 'preview', loadChildren: 'app/preview/preview.module#PreviewModule' },
+  { path: '', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PrismicModule } from './prismic/prismic.module';
+import { CookieModule } from 'ngx-cookie';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    CookieModule.forRoot(),
+    PrismicModule
   ],
   providers: [],
   bootstrap: [AppComponent]

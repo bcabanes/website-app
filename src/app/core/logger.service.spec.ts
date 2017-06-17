18019936651a5bca06b66eabaa39477109da664b
bcabanes <contact@benjamincabanes.com>
2017-06-17 15:31:22 -0400

    feat(core): add core module with logger service & tests

---
+++ src/app/core/logger.service.spec.ts
@@ -0,0 +1,41 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { LoggerService } from './logger.service';
+
+describe('core: LoggerService', () => {
+  beforeEach(() => {
+    // spy
+    spyOn(console, 'log');
+    spyOn(console, 'error');
+    spyOn(console, 'warn');
+
+    TestBed.configureTestingModule({
+      providers: [LoggerService]
+    });
+  });
+
+  describe('api', () => {
+    it('should have defined methods', inject([LoggerService], (logger: LoggerService) => {
+      expect(logger.log).toBeDefined();
+      expect(logger.warn).toBeDefined();
+      expect(logger.error).toBeDefined();
+    }));
+  });
+
+  describe('log', () => {
+    it('should log', inject([LoggerService], (logger: LoggerService) => {
+      logger.log('something to log');
+      expect(console.log).toHaveBeenCalledWith(['log']);
+    }));
+
+    it('should error', inject([LoggerService], (logger: LoggerService) => {
+      logger.error('something to error');
+      expect(console.error).toHaveBeenCalledWith(['error']);
+    }));
+
+    it('should warn', inject([LoggerService], (logger: LoggerService) => {
+      logger.warn('something to warn');
+      expect(console.warn).toHaveBeenCalledWith(['warn']);
+    }));
+  });
+});

469716bc9774ca21972a8a84903aaa180735a9fa
bcabanes <contact@benjamincabanes.com>
2017-07-31 16:17:45 -0400

    test(helpers): simplify type tests

--- src/app/shared/services/helpers/types.spec.ts
+++ src/app/shared/services/helpers/types.spec.ts
@@ -1,21 +1,21 @@
 import { type } from './type';
 
 describe('HELPER - Type', () => {
+  const typeName = '[TEST] My super Type';
+
   it('should accept unique type', () => {
-    const expectedResult = '[TEST] My super Type';
-    expect(() => type(expectedResult)).not.toThrowError();
-    expect(() => type(expectedResult)).toBeTruthy();
+    expect(() => type(typeName)).not.toThrow();
+    expect(() => type(typeName)).toBeTruthy();
 
     const types = [ '[TEST] type 1', '[TEST] type 2', '[TEST] type 3'];
     types.forEach(currentType => {
-      expect(() => type(currentType)).not.toThrowError();
+      expect(() => type(currentType)).not.toThrow();
       expect(() => type(currentType)).toBeTruthy();
     });
   });
 
   it('should not accept duplicated type', () => {
-    const expectedResult = '[TEST] My super Type';
-    expect(() => type(expectedResult))
-      .toThrow(new Error(`Action type "${expectedResult}" is not unique`));
+    expect(() => type(typeName)).toThrowError(`Action type "${typeName}" is not unique`);
+    expect(() => type(typeName)).toThrow();
   });
 });

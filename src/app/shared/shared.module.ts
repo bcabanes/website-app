ea1fb97e5d6966f621be896fa37327ca73ede5d4
bcabanes <contact@benjamincabanes.com>
2017-10-09 16:40:24 -0400

    feat(shared): add flex layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 const MODULES: any[] = [
   CommonModule,
   HttpClientModule,
-  RouterModule
+  RouterModule,
+
+  FlexLayoutModule
 ];
 
 @NgModule({

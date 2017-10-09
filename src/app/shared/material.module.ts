6e8b501306390a7d4e98505e86e903347f67a6d7
bcabanes <contact@benjamincabanes.com>
2017-10-09 18:28:42 -0400

    feat(style-guide): add custom icons

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -21,11 +21,23 @@ export class MaterialModule {
 
   constructor(private iconRegistry: MatIconRegistry,
               private sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon('facebook',
-      sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/facebook.svg'));
-    iconRegistry.addSvgIcon('twitter',
-      sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/twitter.svg'));
-    iconRegistry.addSvgIcon('slack',
-      sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/slack.svg'));
+    this.registerIcons();
+  }
+
+  private registerIcons(): void {
+    this.iconRegistry.addSvgIcon('calendar',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/calendar.svg'));
+    this.iconRegistry.addSvgIcon('cross',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/cross.svg'));
+    this.iconRegistry.addSvgIcon('facebook',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/facebook.svg'));
+    this.iconRegistry.addSvgIcon('locator',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/locator.svg'));
+    this.iconRegistry.addSvgIcon('open',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/open.svg'));
+    this.iconRegistry.addSvgIcon('slack',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/slack.svg'));
+    this.iconRegistry.addSvgIcon('twitter',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/twitter.svg'));
   }
 }

20f2b056bbe6a02e80b1e66f3ed3efc5bd2aba2e
bcabanes <contact@benjamincabanes.com>
2017-12-02 17:39:52 -0500

    test(shared): add nav-bar test

--- src/app/shared/nav-bar/nav-bar.component.spec.ts
+++ src/app/shared/nav-bar/nav-bar.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { LocalizeRouterModule, LocalizeRouterService } from 'localize-router';
 // app
 import { NavBarComponent } from './nav-bar.component';
 
@@ -10,7 +11,13 @@ describe('NavBarComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ NavBarComponent ],
-      imports: [ RouterTestingModule]
+      imports: [ RouterTestingModule ],
+      providers: [
+        {
+          provide: LocalizeRouterService,
+          useValue: jasmine.createSpyObj('localizeRouterService', [ 'translateRoute' ])
+        }
+      ]
     })
       .compileComponents();
   }));

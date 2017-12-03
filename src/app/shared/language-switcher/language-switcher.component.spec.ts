85fd4006b0295592d349d11efb5286708fb3e3f4
bcabanes <contact@benjamincabanes.com>
2017-12-03 16:18:50 -0500

    feat(shared): add language switcher component

---
+++ src/app/shared/language-switcher/language-switcher.component.spec.ts
@@ -0,0 +1,36 @@
+import { CommonModule, Location } from '@angular/common';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { LocalizeRouterModule } from 'localize-router';
+// app
+import { LanguageSwitcherComponent } from './language-switcher.component';
+
+describe('LanguageSwitcherComponent', () => {
+  let component: LanguageSwitcherComponent;
+  let fixture: ComponentFixture<LanguageSwitcherComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LanguageSwitcherComponent ],
+      imports: [
+        CommonModule,
+        LocalizeRouterModule.forRoot([ { path: 'dummy' } ]),
+        TranslateModule.forRoot()
+      ],
+      providers: [ Location ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LanguageSwitcherComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});

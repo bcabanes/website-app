1e19674cb5738dff05da28866f2270a4d20ae7be
bcabanes <contact@benjamincabanes.com>
2017-12-03 16:26:24 -0500

    test(switcher-component): passing

--- src/app/shared/language-switcher/language-switcher.component.spec.ts
+++ src/app/shared/language-switcher/language-switcher.component.spec.ts
@@ -1,6 +1,7 @@
 import { CommonModule, Location } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { LocalizeRouterModule } from 'localize-router';
 // app
@@ -16,9 +17,9 @@ describe('LanguageSwitcherComponent', () => {
       imports: [
         CommonModule,
         LocalizeRouterModule.forRoot([ { path: 'dummy' } ]),
-        TranslateModule.forRoot()
+        TranslateModule.forRoot(),
+        RouterTestingModule
       ],
-      providers: [ Location ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
       .compileComponents();
